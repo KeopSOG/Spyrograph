c502ab9a6f8b8f753dc94661cbae18cbbea19dac
Mihai Anton <161769524+KeopSOG@users.noreply.github.com>
2024-03-02 22:43:57 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import turtle
+from turtle import Turtle, Screen
+import random
+timmy = Turtle()
+turtle.colormode(255)
+timmy.speed(0)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+def draw_spirograph(size):
+    for i in range(int(360 / size)):
+        timmy.color(random_color())
+        timmy.circle(100)
+        timmy.setheading(timmy.heading() + size)
+
+
+draw_spirograph(5)
+
+screen = Screen()
+screen.exitonclick()
